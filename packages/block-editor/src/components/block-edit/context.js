a60dcc0eb861b4241b90ae375d611d254229ee78
Jorge <jorge.costa@developer.pt>
2019-10-17 20:55:16 +0100

    Add class mechanism for preset gradients.

--- packages/block-editor/src/components/block-edit/context.js
+++ packages/block-editor/src/components/block-edit/context.js
@@ -6,18 +6,22 @@ import { noop } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { createContext } from '@wordpress/element';
+import { createContext, useContext } from '@wordpress/element';
 import { createHigherOrderComponent } from '@wordpress/compose';
 
-const { Consumer, Provider } = createContext( {
+const Context = createContext( {
 	name: '',
 	isSelected: false,
 	focusedElement: null,
 	setFocusedElement: noop,
 	clientId: null,
 } );
+const { Provider, Consumer } = Context;
 
 export { Provider as BlockEditContextProvider };
+export function useBlockEditContext() {
+	return useContext( Context );
+}
 
 /**
  * A Higher Order Component used to inject BlockEdit context to the
@@ -27,7 +31,7 @@ export { Provider as BlockEditContextProvider };
  *                                     expected to return object of props to
  *                                     merge with the component's own props.
  *
- * @return {WPComponent} Enhanced component with injected context as props.
+ * @return {Component} Enhanced component with injected context as props.
  */
 export const withBlockEditContext = ( mapContextToProps ) => createHigherOrderComponent( ( OriginalComponent ) => {
 	return ( props ) => (
@@ -46,9 +50,9 @@ export const withBlockEditContext = ( mapContextToProps ) => createHigherOrderCo
  * A Higher Order Component used to render conditionally the wrapped
  * component only when the BlockEdit has selected state set.
  *
- * @param {WPComponent} OriginalComponent Component to wrap.
+ * @param {Component} OriginalComponent Component to wrap.
  *
- * @return {WPComponent} Component which renders only when the BlockEdit is selected.
+ * @return {Component} Component which renders only when the BlockEdit is selected.
  */
 export const ifBlockEditSelected = createHigherOrderComponent( ( OriginalComponent ) => {
 	return ( props ) => (
